ddd1720e8b3240cacd0ea5af4c42c64f219c773d
JorgeLaraR <140337814+JorgeLaraR@users.noreply.github.com>
2023-07-30 11:06:58 -0600

    Declaracion de variables JorgeLara

---
+++ U20230258_Semana2.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DECLARACION DE VARIABLES
+
+            //ITEMS 1
+
+            string primerNombre="Jorge";
+
+            string segundoNombre="Humberto";
+
+            string primerApellido="Lara";
+
+            string segundoApellido="Rodezno";
+
+
+            //ITEMS 2
+
+            int edadMia= 23;
+
+            int edadPapa= 60;
+
+            int edadMama= 55;
+
+
+            //ITEMS 3
+
+            float mesadaSemanal= 15.50;
+            
+            float desayunosUniversidad= 2.25;
+
+            float metrocentroCine= 4.99;
+
+
+            //ITEMS 4
+
+            bool activoMateria= true;
+            
+            bool becado= false;
+
+
+
+            //AUTOEVLUACION 10
+
+
+
+        }
+        
+
+
+
+
+    }
+}
